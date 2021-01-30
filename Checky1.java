9d920d9d5e7fbde7810035be0548d4b3224c6203
MohanSharmav <45537890+MohanSharmav@users.noreply.github.com>
2021-01-30 18:04:24 +0530

    Add files via upload

---
+++ Checky1.java
@@ -0,0 +1,16 @@
+class Checky1
+{
+    public static void main(String args[])
+    {
+        String str1="hello";
+        String str2="olleh";
+    
+       System.out.println(" "+str1+str2);
+         System.out.println(str1.length()==str2.length());
+       System.out.println((str1+str2));
+      String str3=str1+str2;
+      System.out.println(" nodu guru"+str3);
+      System.out.println(str3.indexOf(str2)!=-1);
+     System.out.println(str3.indexOf(str2));
+    }
+}
---
+++ Ify.java
@@ -0,0 +1,11 @@
+class Ify
+{
+    public static void main(String args[])
+    {
+        int i=0;
+        if(i==0)
+         System.out.print("i same");
+                      else
+             System.out.print("not");
+    }
+}
